aedffa9256cf25f6e537be35edb6ea67713a7abc
Pasindu G <pasindug199@gmail.com>
2026-09-09 23:33:11 +0530

    Modified class for logout test and created logout test with assertions

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -5,7 +5,10 @@ class Login {
         this.page = page;
         this.username = page.getByPlaceholder("Username");
         this.password = page.getByPlaceholder("Password");
-        this.loginButton = page.getByRole('Button', {name: "Login"});
+        this.loginButton = page.getByRole('button', {name: "Login"});
+        this.dropdownmenu = page.getByAltText("profile picture");
+        //this.dropdown = page.locator(".oxd-dropdown-menu");
+        this.logout = page.getByRole('menuitem', { name: 'Logout' });
 
     }
 
@@ -13,6 +16,12 @@ class Login {
         await this.username.fill(username);
         await this.password.fill(password);
         await this.loginButton.click();
+    }
+
+    async logoutFromApp(){
+        await this.dropdownmenu.click();
+        //await this.dropdown.click();
+        await this.logout.click();
     }
 }
 
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,44 +1,56 @@
-const {test, expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
 const loginData = require("../data/logindata.json");
-import Login from "../pages/loginPage.js";
+const Login = require("../pages/loginPage.js");
 
+test.describe("Login Tests", function () {
 
-test.describe("Login Tests", function() {
+    for (let i = 0; i < loginData.length; i++) {
 
-
-    for (let i =0; i < loginData.length; i++) {
-
-
-        test(`Login with credentials - ${loginData[i].id}`, async ({page})=>{
+        test(`Login with credentials - ${loginData[i].id}`, async ({ page }) => {
 
             const loginpage = new Login(page);
 
-            await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+            await page.goto(
+                "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+            );
 
-            await loginpage.loginToApp(loginData[i].username, loginData[i].password);
+            await loginpage.loginToApp(
+                loginData[i].username,
+                loginData[i].password
+            );
 
             if (loginData[i].id === "Valid login") {
 
-                await expect(page).toHaveURL("/dashboard/");
+                await expect(page).toHaveURL(
+                    "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+                );
 
-            } else if (loginData[i].id === "Empty Credentials") {
+            } else if (loginData[i].id === "Empty credentials") {
 
-                await expect(page.getByText("Required")).toBeVisible();
-
+                await expect(page.getByText("Required")).toHaveCount(2);
 
             } else {
 
-                await expect(page.getByText("Invalid credentials")).toBeVisible();
+                await expect(
+                    page.getByText("Invalid credentials", { exact: true })
+                ).toBeVisible();
             }
-
-
-
-            
-        })
+        });
     }
 
+    test("Logout from application", async ({ page }) => {
 
+        const loginpage = new Login(page);
 
+        await page.goto(
+            "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+        );
 
-})
+        await loginpage.loginToApp("admin", "admin123");
+
+        await loginpage.logoutFromApp();
+
+        await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    });
+});
