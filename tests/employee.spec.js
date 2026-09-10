3ec45dca6cc84c807e1375f0f42ccfde0c12bdfc
Pasindu G <pasindug199@gmail.com>
2026-09-10 12:53:32 +0530

    Created test script to add employee and added assertion

---
+++ tests/employee.spec.js
@@ -0,0 +1,41 @@
+const {test, expect} = require('@playwright/test');
+
+const Login = require("../pages/loginPage.js");
+const Employee = require("../pages/employeePage.js");
+
+const username = "Admin";
+const password = "admin123";
+
+test.describe("Employee Tests", function (){
+
+    test.beforeEach(async ({page}) =>{
+
+        const loginpage = new Login(page);
+
+        await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        await loginpage.loginToApp(username,password);
+    });
+
+
+    test("Add Employee details", async ({page})=>{
+
+        const employeepage = new Employee(page);
+
+        await employeepage.clickPIM();
+        await employeepage.clickAddEmployee();
+        await employeepage.addEmployeeDetails();
+        
+        await expect(page.getByText("Successfully Saved", { exact: true })).toBeVisible();
+
+        
+
+    });
+
+    
+
+
+
+
+
+})
