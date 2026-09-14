12ee53c73b16ca62f894da6d59c8de67a72409b3
Pasindu G <pasindug199@gmail.com>
2026-09-14 19:51:23 +0530

    Created test to add admin

--- pages/adminPage.js
+++ pages/adminPage.js
@@ -3,17 +3,17 @@ class Admin {
     constructor(page) {
 
         this.page = page;
-        this.admin = page.geByRole("link", {name:"Admin"});
+        this.admin = page.getByRole("link", {name:"Admin"});
         this.add = page.getByRole("button", {name:"Add"});
-        this.userRole = page.locator(".select-text-input").first();
+        this.userRole = page.locator(".oxd-select-text-input").nth(0);
         this.dropdownMenu = page.locator(".oxd-select-dropdown");
-        this.employeeName = getByPlaceholder("Type for hints...");
-        this.employeeSelect = page.locator(".oxd-select-dropdown");
-        this.status = page.locator(".select-text-input").second();
+        this.employeeName = page.getByPlaceholder("Type for hints...");
+        this.employeeSelect = page.locator(".oxd-autocomplete-dropdown");
+        this.status = page.locator(".oxd-select-text-input").nth(1);
         this.statusDropdown = page.locator(".oxd-select-dropdown");
-        this.username = page.locator("input.oxd-input.oxd-input--active").second();
-        this.password = page.locator("input.oxd-input.oxd-input--active").third();
-        this.confirmPassword = page.locator("input.oxd-input.oxd-input--active").second();
+        this.username = page.locator("input.oxd-input.oxd-input--active").nth(1);
+        this.password = page.locator("input[type='password']").nth(0);
+        this.confirmPassword = page.locator("input[type='password']").nth(1);
         this.saveButton = page.getByRole("button", {name:"Save"});
 
 
@@ -33,17 +33,19 @@ class Admin {
 
         await this.userRole.click();
         await this.dropdownMenu.getByText("Admin", {exact:true}).click();
-        await this.employeeName.fill("Paul Wood Smith");
-        await this.employeeSelect.click();
+        await this.employeeName.fill("James Butler");
+        await this.employeeSelect.getByText("James Butler", { exact: true }).click();
         await this.status.click();
         await this.statusDropdown.getByText("Enabled", {exact:true}).click();
-        await this.username("paul12");
-        await this.password("paul123@#");
-        await this.confirmPassword("paul123@#");
+        await this.username.fill("james12");
+        await this.password.fill("james123@#");
+        await this.confirmPassword.fill("james123@#");
         await this.saveButton.click();
-        await this.saveButton.click();
+        
 
     }
 
 
 }
+
+module.exports = Admin;
---
+++ tests/admin.spec.js
@@ -0,0 +1,30 @@
+const {test, expect} = require('@playwright/test');
+
+const Login = require("../pages/loginPage.js");
+const Admin = require("../pages/adminPage.js");
+
+const username = "Admin";
+const password = "admin123";
+
+test.describe("Admin Tests", function(){
+
+    test.beforeEach(async ({page})=>{
+
+        const loginpage = new Login(page);
+
+        await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        await loginpage.loginToApp(username,password);
+
+    });
+
+    test("Add Admin", async ({page})=>{
+
+        const adminpage = new Admin(page);
+
+        await adminpage.clickAdmin();
+        await adminpage.addAdmin();
+        await adminpage.addAdminDetails();
+        
+    })
+})
